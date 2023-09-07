72afe3ee23219c5c8082b147706daf90fdb756c4
alex.koukis <alexandroskoukis@gmail.com>
2023-09-07 15:35:23 +0300

    fix migrations for mysql

--- prisma/migrations/20230615155147_create_session_table/migration.sql
+++ prisma/migrations/20230615155147_create_session_table/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
-CREATE TABLE "Session" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "shop" TEXT NOT NULL,
-    "state" TEXT NOT NULL,
-    "isOnline" BOOLEAN NOT NULL DEFAULT false,
-    "scope" TEXT,
-    "expires" DATETIME,
-    "accessToken" TEXT NOT NULL,
-    "userId" BIGINT
+CREATE TABLE `Session` (
+    `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+    `shop` VARCHAR(255) NOT NULL,
+    `state` VARCHAR(255) NOT NULL,
+    `isOnline` BOOLEAN NOT NULL DEFAULT false,
+    `scope` VARCHAR(255),
+    `expires` DATETIME,
+    `accessToken` VARCHAR(255) NOT NULL,
+    `userId` BIGINT
 );
